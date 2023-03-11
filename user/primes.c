3041769c049bf158e75385e2c6da43291466c9b5
cserycx <cserycx@163.com>
2023-03-11 08:05:31 +0800

    just unfinished primes

--- user/primes.c
+++ user/primes.c
@@ -12,55 +12,17 @@
 */
 int status ;
 char buf[1] = {0};
-int p[2];
 
 int 
 main(){
     const int begin = 2;
     const int end = 35;
+    int p[2];
+    pipe(p);
     for(int i=begin;i<=end;++i){
-        *buf = begin;
-        if(i == begin){
-            printf("prime %d\n",i);
-            continue;
-        }
-        if(i % (*buf) == 0){
-            continue;
-        }
-        else{
-            pipe(p);
-            write(p[1],buf,1);
-            if(fork() == 0){
-                read(p[0],buf,1);
-                if(*buf < i && i%(*buf) == 0){
-                    close(p[0]);
-                    close(p[1]);
-                    exit(1);
-                }
-                else if((*buf) == i){
-                    printf("i is %d\n",i);
-                    close(p[0]);
-                    close(p[1]);
-                    exit(0);
-                }
-                else{
-                    (*buf) += 1;
-                    fork();
-                    exit(0);
-                }
-            }else{
-                wait(&status);
-                if(!status){
-                    printf("status %d\n",status);
-                    printf("prime %d\n",i);
-                    close(p[0]);
-                    close(p[1]);
-                }else{
-                    close(p[0]);
-                    close(p[1]);
-                }
-            }
-        }
+        buf[0] = begin;
+        write(p[1],buf,1);
+        if(fork() == )
     }
     exit(0);
 }
